e1f8e56aec091623270bb305fc1dc9456597f90f
bednsasha <bednsasha@gmail.com>
2025-05-31 16:24:05 +0300

    1 попытка создания шаблонов и моделей

--- Memora/main/models.py
+++ Memora/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Note(models.Model):
+    name=models.CharField(max_length=200)
+    text = models.TextField(blank=True, null=True)
+    slug=models.SlugField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+ 
+    class Meta:
+        ordering = ('name',)
+       
+    def __str__(self):
+        return self.name
+    
+def get_absolute_url(self):
+    return reverse('main:note_detail',
+                   args=[self.slug])
